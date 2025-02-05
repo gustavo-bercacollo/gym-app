c7556d6801e59ad0696323a15d533cb3548ae448
Gustavo Berçacollo Vilela <gustavovilela802@gmail.com>
2025-02-05 18:45:23 -0300

    chore(fonts): add custom font to the project

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
+
+ const [fontsLoaded] =  useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+
+
   return (
-    <View>
-      <Text>home</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {fontsLoaded ? <Text>home</Text>: <View />} 
     </View>
   );
 }
