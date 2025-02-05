dff763a76ca4d3855a9cbc925b5b454e49bf6bef
Gustavo Berçacollo Vilela <gustavovilela802@gmail.com>
2025-02-05 18:51:35 -0300

    style(status bar): customize appearance

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
@@ -9,6 +9,12 @@ export default function App() {
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+
+      <StatusBar 
+      barStyle="light-content" 
+      backgroundColor={'transparent'}
+      translucent
+      />
       {fontsLoaded ? <Text>home</Text>: <View />} 
     </View>
   );
