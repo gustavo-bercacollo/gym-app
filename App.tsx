6794510fb36d1e92f671a44760e81466fa39ac1b
Gustavo Berçacollo Vilela <gustavovilela802@gmail.com>
2025-02-05 21:02:00 -0300

    "feat(loading): add loading component to show a spinner while fonts are loading
    This component improves user experience by displaying a loading spinner when fonts are not yet loaded."

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
-import { View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { GluestackUIProvider, Center, Text } from '@gluestack-ui/themed';
 import { config } from "./config/gluestack-ui.config"
+import { Loading } from '@components/loading';
  
 export default function App() {
  
@@ -20,7 +21,7 @@ export default function App() {
       {fontsLoaded ? 
       <Center flex={1} bg="$gray700">
         <Text>home</Text>  
-      </Center>: <View />} 
+      </Center>: <Loading />} 
   
   </GluestackUIProvider>
   );
