0c9782bf481a4f4dbb5b0d61a33f07ec206fda55
Gustavo Berçacollo Vilela <gustavovilela802@gmail.com>
2025-02-10 21:54:45 -0300

    feat(Home): add group selection logic
    This commit implements state management to handle group selection in the Home screen, allowing users to toggle between different groups.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,33 @@
-import { VStack } from "@gluestack-ui/themed";
+import { HStack, VStack } from "@gluestack-ui/themed";
 import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
+import { useState } from "react";
 
 export function Home() {
+
+  const[groupSelect, setGroupSelect] = useState("costa")
+  
+
+
   return(
     <VStack flex={1}>
       <HomeHeader/>
-      <Group name="Costas" isActive={false} />
+
+      <HStack>
+      <Group name="costa" 
+      isActive={groupSelect === "costa"}
+      onPress={() => setGroupSelect("costa")}
+      />
+
+      <Group name="ombro" 
+      isActive={groupSelect === "ombro"}
+      onPress={() => setGroupSelect("ombro")}
+      />
+
+
+      
+      </HStack>
+      
     </VStack>
   )
 }
