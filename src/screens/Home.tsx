9dca2cbed82270537e7501d51526be3454966f50
Gustavo Berçacollo Vilela <gustavovilela802@gmail.com>
2025-02-11 01:10:00 -0300

    feat(Home): implement dynamic group selection with FlatList
    This commit updates the Home screen to render group buttons dynamically using FlatList, improving scalability and maintainability.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,9 +2,11 @@ import { HStack, VStack } from "@gluestack-ui/themed";
 import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
 import { useState } from "react";
+import { FlatList } from "react-native";
 
 export function Home() {
 
+  const[groups, setGroups] = useState(["grupo 1", "grupo 2", "grupo 3", "grupo 4" ])
   const[groupSelect, setGroupSelect] = useState("costa")
   
 
@@ -13,21 +15,22 @@ export function Home() {
     <VStack flex={1}>
       <HomeHeader/>
 
-      <HStack>
-      <Group name="costa" 
-      isActive={groupSelect === "costa"}
-      onPress={() => setGroupSelect("costa")}
+      <FlatList
+      data={groups}
+      keyExtractor={(item) => item}
+      renderItem={({item}) => (
+      <Group 
+      name={item} 
+      isActive={groupSelect === item}
+      onPress={() => setGroupSelect(item)}
+      />
+      )}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{ paddingHorizontal: 32 }}
+      style={{ marginVertical: 40, maxHeight: 44, minWidth:44 }}
       />
 
-      <Group name="ombro" 
-      isActive={groupSelect === "ombro"}
-      onPress={() => setGroupSelect("ombro")}
-      />
-
-
-      
-      </HStack>
-      
     </VStack>
   )
 }
