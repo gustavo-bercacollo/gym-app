0a4bd08ea6aac5d58417928f57c5ef5b8f14f6ac
Gustavo Berçacollo Vilela <gustavovilela802@gmail.com>
2025-02-13 19:50:02 -0300

    feat(Exercise): add header with navigation and exercise details
    This commit adds a header to the Exercise screen, including a go-back button, exercise name, and muscle group name.

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,11 +1,58 @@
-import { Center, Text } from "@gluestack-ui/themed";
+import { useNavigation } from "@react-navigation/native";
+import { VStack, Icon, HStack, Heading, Text } from "@gluestack-ui/themed";
+import { TouchableOpacity } from "react-native";
+import { ArrowLeft} from "lucide-react-native"
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
+import BodySvg from "@assets/body.svg"
+
 
 export function Exercise() {
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleGoBack() {
+    navigation.goBack(); 
+  }
+
   return(
-    <Center flex={1}>
-      <Text>
-       Exercise
-      </Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack 
+      px="$8" 
+      bg="$gray600" 
+      pt="$12">
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon as={ArrowLeft} 
+          color="$green500" 
+          size="xl" />
+        </TouchableOpacity>
+
+        <HStack 
+        justifyContent="space-between" 
+        alignItems="center" 
+        mt="$4" 
+        mb="$8"
+        >
+          <Heading 
+          color="$gray100"
+          fontFamily="$heading"
+          fontSize="$lg"
+          flexShrink={1}
+          >
+            Puxada frontal
+          </Heading>
+
+          <HStack>
+            <BodySvg/>
+            <Text 
+            color="$gray200"
+            ml="$1"
+            textTransform="capitalize"
+            >
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
+      </VStack>
+    </VStack>
   )
 }
